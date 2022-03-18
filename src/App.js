6c8522f605b4f60c88ac8509490c7dddc4826bf6
ANKE-SaiPrasad <89138270+ankesai8@users.noreply.github.com>
2022-03-18 09:09:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,15 +4,10 @@ import Admin from "./routes/Admin";
 import User from "./routes/User";
 
 function App() {
-  // const navigate = useNavigate();
-  // if (JSON.parse(localStorage.getItem("userData")) !== null) {
-  //   navigate("/jobs");
-  // }
-  return (
-    <SnackbarProvider>
+ return (
+   
       <Admin />
       <User />
-    </SnackbarProvider>
   );
 }
 
