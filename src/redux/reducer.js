e5e53d43aa16b1b7c29378b070957693bbab34a8
ankesai6 <96909598+ankesai6@users.noreply.github.com>
2022-03-19 18:30:59 +0530

    Update reducer.js

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,12 +1,8 @@
 import { combineReducers } from "redux";
-
-// reducer import
-
 import userReducer from "./userReducer";
 import jobReducer from "./jobReducer";
 
-// ===========================|| COMBINE REDUCER ||=========================== //
-
+// ===========================COMBINE REDUCER=========================== //
 const reducer = combineReducers({ userReducer, jobReducer });
 
 export default reducer;
